e2029e5ee6afe6edeee2e0e8db5c6dca991ae1c2
alireza23 <ali.haghighi1367@gmail.com>
2021-10-02 00:24:32 -0700

    ggg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useCallback, useRef } from "react";
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import image1 from "./base_bw.png"; // with import
+
 function App() {
+  const imgRef = useRef();
+  const onUpdate = useCallback(({ x, y, scale }) => {
+    const { current: img } = imgRef;
+
+    if (img) {
+      const value = make3dTransformValue({ x, y, scale });
+
+      img.style.setProperty("transform", value);
+    }
+  }, []);
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <QuickPinchZoom onUpdate={onUpdate}>
+      <img ref={imgRef} src={image1} />
+    </QuickPinchZoom>
   );
 }
-
 export default App;
-//code
